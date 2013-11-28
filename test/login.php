2b0c8e9e7cc9b149d9668e3daef5263484131788
Maik Zeyen <maik_zeyen@web.de>
2013-11-28 15:33:41 +0100

    add dragabble to login dialog

--- test/login.php
+++ test/login.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
     <link rel="stylesheet" href="../test/css/styles.css">
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+	<script type="text/javascript">
+ 
+$( init );
+ 
+function init() {
+  $('#login').draggable();
+ }
+</script>
   <head>
     <title>
       Login
     </title>
   </head>
   <body>
-  <section class="login">
-  <div class="title">Login</div>
+  <section class="login" id="login">
+  <div class="title" >Login</div>
     <form action="../test/includes/login.php" method="post">
       <fieldset>
 		<?php include ('../test/templates/user_form.php'); ?>
--- test/register.php
+++ test/register.php
@@ -7,12 +7,17 @@
     </title>
   </head>
   <body>
-  <section class="login">
+  <section class="register">
   <div class="title">Register new account</div>
     <form action="/test/includes/register.php" method="post">
       <fieldset>
         <?php include "../test/templates/user_form.php" ?>
-	<input type="text" required title="Email required" name="email" id="email" placeholder="Email" data-icon="x">
+		<input type="text" required title="Email required" name="email" id="email" placeholder="Email" data-icon="x">
+		<div class="reglink">
+			<div class="col">
+				<a href="../test/index.php" title="">Cancel</a>
+			</div>
+		</div>
         <input type="submit" class="button" value="Registrieren" />
       </fieldset>
     </form>
