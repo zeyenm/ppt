de1076cca4eab41219d8da012cff32d7b3055e54
Maik Zeyen <maik_zeyen@web.de>
2013-11-28 13:42:46 +0100

    Create repo

---
+++ includes/login.php
@@ -0,0 +1,27 @@
+<?php   
+  //Script by http://decodingtutorials.wordpress.com  
+
+  session_start(); //Session starten um Session-Variablen initialisieren zu können
+
+  include("connect.php"); 
+
+  $connection  = mysql_connect ($db_Host.$db_HostPort, $db_User, $db_UserPW);     
+  mysql_select_db ($db_DBName , $connection);   
+  $name = $_POST["name"];   
+  $pass = $_POST["pass"];   
+echo $name;
+  $sql = "SELECT id, usr FROM users WHERE (usr = '$name' AND pass = '$pass');";   
+  $result = mysql_query($sql);   
+  $row = mysql_fetch_array($result);  
+  
+  if (mysql_num_rows($result)==1) {     
+    $_SESSION["id"] = $row["id"];     
+    $_SESSION["name"] = $row["usr"];    
+    die(header("location: member.php"));   
+  }  
+
+  else {    
+    die(header("location: ../register.html"));  
+  } 
+
+?>
---
+++ includes/register.php
@@ -0,0 +1,17 @@
+<?php
+  //Script by http://decodingtutorials.wordpress.com
+
+	
+	$ip = getenv('HTTP_CLIENT_IP')?:
+  include("connect.php");
+
+  $connection  = mysql_connect($db_Host.$db_HostPort, $db_User, $db_UserPW); 
+  mysql_select_db ($db_DBName , $connection);
+
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $pass = $_POST["pass"]; //Passwort in verschlüsselter Form in die Datenbank speichern
+  $sql = "INSERT INTO users (id , usr , pass, email, regIP, dt) VALUES ( NULL ,'$name','$pass', '$email', '', '0000-00-00 00:00:00');";
+  mysql_query($sql) or die(mysql_error());
+  die(header("location: ../index.php")); //Benutzer zur Login-Seite weiterleiten
+?>
